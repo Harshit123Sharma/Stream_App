a4a57b9db1e460bf5eb4a371079a83229bd71ac7
harshit2001sharma@gmail.com <44293679+Harshit123Sharma@users.noreply.github.com>
2021-07-09 22:42:24 +0530

    Fixed Errors origin

--- client/src/reducers/streamReducer.js
+++ client/src/reducers/streamReducer.js
@@ -5,27 +5,22 @@ import {
   FETCH_STREAMS,
   FETCH_STREAM,
   DELETE_STREAM,
-  EDIT_STREAM
+  EDIT_STREAM,
 } from "../actions/types";
 
 export const streamReducer = (state = {}, action) => {
-    switch (action.type) {
-      case FETCH_STREAM:
-        return { ...state, [action.payload.id]: action.payload.id };
-
-      case CREATE_STREAM:
-        return { ...state, [action.payload.id]: action.payload.id };
-
-      case EDIT_STREAM:
-        return { ...state, [action.payload.id]: action.payload.id };
-
-      case DELETE_STREAM:
-        return _.omit(state, action.payload);
-
-      case FETCH_STREAMS:
-        return { ...state, ..._.mapKeys(action.payload, "id") };
-
-      default:
-        return state;
-    }
-}
+  switch (action.type) {
+    case FETCH_STREAMS:
+      return { ...state, ..._.mapKeys(action.payload, "id") };
+    case FETCH_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case CREATE_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case EDIT_STREAM:
+      return { ...state, [action.payload.id]: action.payload };
+    case DELETE_STREAM:
+      return _.omit(state, action.payload);
+    default:
+      return state;
+  }
+};
